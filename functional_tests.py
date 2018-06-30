15b5c56b61f848061d438a170367b630edd8404a
Nicholas Green <me@nick.green>
2018-06-30 23:33:56 +1000

    Now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(executable_path=r"C:\Python\Projects\TDD\superlists\chromedriver.exe")
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(executable_path=r"C:\Python\Projects\TDD\superlists\chromedriver.exe")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_userTest(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
